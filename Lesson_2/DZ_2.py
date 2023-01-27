faeca9ac005d84b75b4fd7230b5cee4708f5e1df
Excellentc <kochapp@gmail.com>
2023-01-28 00:42:02 +0200

    refactoring 1 dz

--- Lesson_2/DZ_2.py
+++ Lesson_2/DZ_2.py
@@ -7,13 +7,9 @@ word = input('Input you word, please: ')
 if word.isalpha() is False:
     print('You entered an invalid value.\nTry again. Only letters, only 1 word.')
 else:
-    if word.strip(' ').find(' ') >= 0:
-        print('You entered more than one word. \nTry again.')
-    else:
-        len_word = str(len(word.strip(' ')))
-        screen_input = 'Word \'{}\' has {} letters"'
-        result = screen_input.format(word.title(), len_word)
-        print(result)
+    screen_input = 'Word \'{}\' has {} letters"'
+    result = screen_input.format(word.title(), len(word))
+    print(result)
 
 """Напишіть программу "Касир в кінотеватрі", яка попросіть користувача ввести свсвій вік
   (можно використати константу або функцію input(), на екран має бути виведено лише одне повідомлення,
@@ -24,20 +20,20 @@ else:
         якщо вік користувача містить цифру 7 - вивести повідомлення "Вам сьогодні пощастить!"
         у будь-якому іншому випадку - вивести повідомлення "А білетів вже немає!" """
 
-# user_age = input('Input your age , please: ')
-# if user_age.isdigit() is False:
-#     print('You entered an invalid value.\nTry again. Input only numbers .')
-# else:
-#     if user_age.find('7') >= 0:
-#         print('You are lucky today!')
-#     user_age1 = int(user_age)
-#     if 0 < user_age1 < 7:
-#         print('Where are your parents?')
-#     elif 7 <= user_age1 < 16:
-#         print('This is an adult movie!')
-#     elif user_age1 > 65:
-#         print('Show your pension certificate!')
-#     elif 16 <= user_age1 <= 65:
-#         print('There are no more tickets!')
-#     elif user_age1 == 0:
-#         print('Zero age! Film is canceled, today we will show - YOU!')
+user_age = input('Input your age , please: ')
+if user_age.isdigit() is False:
+    print('You entered an invalid value.\nTry again. Input only numbers .')
+else:
+    if user_age.find('7') >= 0:
+        print('You are lucky today!')
+    user_age1 = int(user_age)
+    if 0 < user_age1 < 7:
+        print('Where are your parents?')
+    elif 7 <= user_age1 < 16:
+        print('This is an adult movie!')
+    elif user_age1 > 65:
+        print('Show your pension certificate!')
+    elif 16 <= user_age1 <= 65:
+        print('There are no more tickets!')
+    elif user_age1 == 0:
+        print('Zero age! Film is canceled, today we will show - YOU!')
