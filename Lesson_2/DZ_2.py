f11edc63b4da4e64005242a201cf05c124a9a879
Excellentc <kochapp@gmail.com>
2023-01-28 15:53:42 +0200

    Second variant for second task DZ_2.

--- Lesson_2/DZ_2.py
+++ Lesson_2/DZ_2.py
@@ -3,13 +3,13 @@
 наприклад "Word 'Python' has 6 letters".
 Для отримання слова для аналізу скористайтеся константою або функцією input()."""
 
-word = input('Input you word, please: ')
-if word.isalpha() is False:
-    print('You entered an invalid value.\nTry again. Only letters, only 1 word.')
-else:
-    screen_input = 'Word \'{}\' has {} letters"'
-    result = screen_input.format(word.title(), len(word))
-    print(result)
+# word = input('Input you word, please: ')
+# if word.isalpha() is False:
+#     print('You entered an invalid value.\nTry again. Only letters, only 1 word.')
+# else:
+#     screen_input = 'Word \'{}\' has {} letters"'
+#     result = screen_input.format(word.title(), len(word))
+#     print(result)
 
 """Напишіть программу "Касир в кінотеватрі", яка попросіть користувача ввести свсвій вік
   (можно використати константу або функцію input(), на екран має бути виведено лише одне повідомлення,
@@ -20,20 +20,20 @@ else:
         якщо вік користувача містить цифру 7 - вивести повідомлення "Вам сьогодні пощастить!"
         у будь-якому іншому випадку - вивести повідомлення "А білетів вже немає!" """
 
-user_age = input('Input your age , please: ')
-if user_age.isdigit() is False:
-    print('You entered an invalid value.\nTry again. Input only numbers .')
+user_age = input('Input your age, only numbers please: ')
+if not user_age.isdigit():
+    print('You entered an invalid value. Try again. Input only numbers .')
 else:
-    if user_age.find('7') >= 0:
-        print('You are lucky today!')
-    user_age1 = int(user_age)
-    if 0 < user_age1 < 7:
+    user_age_int = int(user_age)                      # Cмена типа для переменной, для будущих арифметических сравнений
+    if user_age.find('7') >= 0:                       # Проверка условия есть ли 7 рка в возрасте
+        print(f'You are will be lucky today')
+    elif 0 < user_age_int < 7:                        # Проверка условия 0 < x < 7
         print('Where are your parents?')
-    elif 7 <= user_age1 < 16:
+    elif 7 <= user_age_int < 16:                      # Проверка условия 7 =< x < 16
         print('This is an adult movie!')
-    elif user_age1 > 65:
-        print('Show your pension certificate!')
-    elif 16 <= user_age1 <= 65:
+    elif 16 <= user_age_int <= 65:                    # Проверка условия 16 <= x <= 65
         print('There are no more tickets!')
-    elif user_age1 == 0:
-        print('Zero age! Film is canceled, today we will show - YOU!')
+    elif 65 < user_age_int < 105:                     # Проверка условия   105 > x > 65
+        print('Show your pension certificate!')
+    elif user_age_int == 0 or user_age_int >= 105:    # Проверка граничных условий: если введен 0 или >= 105 лет
+        print("Liar. People Don't live with this age")
