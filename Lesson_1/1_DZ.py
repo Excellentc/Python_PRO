6e01114311acfd88d1e585f5df8e799d75213315
Excellentc <kochapp@gmail.com>
2023-01-29 19:19:51 +0200

    add brief explanation of actions in print

--- Lesson_1/1_DZ.py
+++ Lesson_1/1_DZ.py
@@ -1,6 +1,6 @@
 """1. Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії
       (+, -, *, / и тд.) для цих чисел."""
-print ("------------------Home Task number 1 (Arithmetic operations)")
+print("------------------Home Task number 1 (Arithmetic operations)")
 first_var = 10
 second_var = 30
 result = first_var + second_var
@@ -20,7 +20,7 @@ print(f'REMAINDER OF DEVISION : {first_var} % {second_var} = ', result)
 
 """ 2. Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 
       Виведіть на екран результат кожного порівняння."""
-print ("------------------Home Task number 2 (Сomparison operations)")
+print("------------------Home Task number 2 (Сomparison operations)")
 first_var = 10
 second_var = 30
 result = first_var < second_var
@@ -38,9 +38,9 @@ print(f'{first_var} >= {second_var} ?', result)
 
 
 """ З. Створіть змінну - резкльтат конкатенації строк "Hello " та "world!".  """
-print ("------------------Home Task number 3 (Concatenation)")
+print("------------------Home Task number 3 (Concatenation)")
 word_1 = "Hello"
 word_2 = "world!"
-print(f'Type 1 : {word_1} {word_2}')
+print(f'Type 1 - concatenation in \'Print\': {word_1} {word_2}')
 result_sentence = word_1 + " " + word_2
-print("Type 2 :", result_sentence)
+print("Type 2 - concatenation to variable:", result_sentence)
