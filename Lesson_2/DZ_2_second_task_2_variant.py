7e5410bc1a4b31cc0020a4a9b802682bcbd18cc3
Excellentc <kochapp@gmail.com>
2023-01-29 18:46:19 +0200

    refactoring, moved the search for "7" in aged to beginning of the script

--- Lesson_2/DZ_2_second_task_2_variant.py
+++ Lesson_2/DZ_2_second_task_2_variant.py
@@ -8,27 +8,20 @@
         у будь-якому іншому випадку - вивести повідомлення "А білетів вже немає!" """
 
 user_age = input('Input your age, only numbers please: ')
+user_age_int = int(user_age)                                # смена типа для переменной, для будущих сравнений
+lucky_guy_msg = ''
+if user_age.find('7') >= 0:
+    lucky_guy_msg = 'You are will be lucky today!'          # присвоение переменной для счастливчика
 if not user_age.isdigit():
     print('You entered an invalid value. Try again. Input only one numbers .')
 else:
-    lucky_guy_msg = 'You are will be lucky today!'      # присвоение переменной для счастливчика
-    user_age_int = int(user_age)                        # смена типа для переменной, для будущих сравнений
     if 0 < user_age_int < 7:                            # Проверка условия 0 < x < 7
         print('Where are your parents?')
     elif 7 <= user_age_int < 16:                        # Проверка условия 7 =< x < 16
-        if user_age.find('7') >= 0:                      # Поиск 7 ки в возрасте
-            print(f'This is an adult movie!  but -  {lucky_guy_msg}')
-        else:
-            print('This is an adult movie!')
+        print(f'This is an adult movie! {lucky_guy_msg}')
     elif 16 <= user_age_int <= 65:                      # Проверка условия 16 <= x <= 65
-        if user_age.find('7') >= 0:                      # Поиск 7 ки в возрасте
-            print(f'There are no more tickets! but - {lucky_guy_msg}')
-        else:
-            print('There are no more tickets!')
+        print(f'There are no more tickets! {lucky_guy_msg}')
     elif 65 < user_age_int < 105:                       # Проверка условия 65 < x < 105
-        if user_age.find('7') >= 0:                      # Поиск 7 ки в возрасте
-            print(f' {lucky_guy_msg} - Show your pension certificate! ')
-        else:
-            print('Show your pension certificate!')
+        print(f' {lucky_guy_msg} Show your pension certificate! ')
     elif user_age_int == 0 or user_age_int >= 105:      # Проверка граничных условий: если введен 0 или >= 105 лет
         print("Liar. People Don't live with this age")
