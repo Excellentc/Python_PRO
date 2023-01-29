848f13468338ba826d91944dfb419ce2ef9d95f7
Excellentc <kochapp@gmail.com>
2023-01-29 19:12:57 +0200

    add normal name for variable calculations taken out of print

--- Lesson_1/1_DZ.py
+++ Lesson_1/1_DZ.py
@@ -1,37 +1,46 @@
 """1. Створіть дві змінні first=10, second=30. Виведіть на екран результат математичної взаємодії
       (+, -, *, / и тд.) для цих чисел."""
 print ("------------------Home Task number 1 (Arithmetic operations)")
-first = 10
-second = 30
-print(f'SUM : {first} + {second} =', first + second)
-print(f'SUBTRACTION : {first} - {second} =', first - second)
-print(f'MULTIPLICATION : {first} * {second} = ', first * second)
-print(f'EXPONENTIATION : {first} ** {second} = ', first ** second)
-print(f'DIVISION : {first} / {second} = ', first / second)
-print(f'INTEGER DIVISION : {second} // {first} = ',  second // first)
-print(f'REMAINDER OF DEVISION : {first} % {second} = ', first % second)
+first_var = 10
+second_var = 30
+result = first_var + second_var
+print(f'SUM : {first_var} + {second_var} =', result)
+result = first_var - second_var
+print(f'SUBTRACTION : {first_var} - {second_var} =', result)
+result = first_var * second_var
+print(f'MULTIPLICATION : {first_var} * {second_var} = ', result)
+result = first_var ** second_var
+print(f'EXPONENTIATION : {first_var} ** {second_var} = ', result)
+result = first_var / second_var
+print(f'DIVISION : {first_var} / {second_var} = ', result)
+result = first_var // second_var
+print(f'INTEGER DIVISION : {second_var} // {first_var} = ',  result)
+result = first_var % second_var
+print(f'REMAINDER OF DEVISION : {first_var} % {second_var} = ', result)
 
 """ 2. Створіть змінну и по черзі запишіть в неї результат порівняння (<, > , ==, !=) чисел з завдання 
       Виведіть на екран результат кожного порівняння."""
 print ("------------------Home Task number 2 (Сomparison operations)")
-c = first < second
-print(f'{first} < {second} ?', c)
-c = first > second
-print(f'{first} > {second} ?', c)
-c = first == second
-print(f'{first} == {second} ?', c)
-c = first != second
-print(f'{first} != {second} ?', c)
-c = first <= second
-print(f'{first} <= {second} ?', c)
-c = first >= second
-print(f'{first} >= {second} ?', c)
+first_var = 10
+second_var = 30
+result = first_var < second_var
+print(f'{first_var} < {second_var} ?', result)
+result = first_var > second_var
+print(f'{first_var} > {second_var} ?', result)
+result = first_var == second_var
+print(f'{first_var} == {second_var} ?', result)
+result = first_var != second_var
+print(f'{first_var} != {second_var} ?', result)
+result = first_var <= second_var
+print(f'{first_var} <= {second_var} ?', result)
+result = first_var >= second_var
+print(f'{first_var} >= {second_var} ?', result)
 
 
 """ З. Створіть змінну - резкльтат конкатенації строк "Hello " та "world!".  """
 print ("------------------Home Task number 3 (Concatenation)")
-a = "Hello"
-b = "world!"
-print(f'Type 1 : {a} {b}')
-c = a + " " + b
-print("Type 3 :", c)
+word_1 = "Hello"
+word_2 = "world!"
+print(f'Type 1 : {word_1} {word_2}')
+result_sentence = word_1 + " " + word_2
+print("Type 2 :", result_sentence)
