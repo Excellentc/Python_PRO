301e1dc042f490d9fe00673d6a6405c5acd3472d
Excellentc <kochapp@gmail.com>
2023-02-05 21:03:45 +0200

    del reverse

--- Lesson_4/DZ_4.py
+++ Lesson_4/DZ_4.py
@@ -38,7 +38,6 @@ def filter_list(data_dict, low_limit, upp_limit):
     for key, value in data_dict.items():
         if low_limit < value < upp_limit:
             string_list.append(key)
-            string_list.reverse()
     return string_list
 
 
